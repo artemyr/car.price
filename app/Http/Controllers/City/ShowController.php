cc654645280550752a56b40a0a1ee5d65e1b0dbe
artemyr <artem.r@affetta.ru>
2022-11-05 23:07:10 +0300

    fix city show controller

--- app/Http/Controllers/City/ShowController.php
+++ app/Http/Controllers/City/ShowController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\City;
 
 use App\Models\City;
+use App\Models\MoreAskedQuestion;
 
 class ShowController extends BaseController
 {
@@ -19,7 +20,8 @@ class ShowController extends BaseController
 
         $articles = $city->articles;
         $reviews = $city->reviews;
+        $moreAskedQuestions = MoreAskedQuestion::all();
 
-        return view('city', compact('city','categories','articles','reviews'));
+        return view('city', compact('city','categories','articles','reviews','moreAskedQuestions'));
     }
 }
