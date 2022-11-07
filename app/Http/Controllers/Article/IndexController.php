ab85d56d1441d0b83ca171fd3af9b603b1062030
artemyr <artem.r@affetta.ru>
2022-11-07 22:28:58 +0300

    [FIX] display articles and reviews

--- app/Http/Controllers/Article/IndexController.php
+++ app/Http/Controllers/Article/IndexController.php
@@ -4,12 +4,15 @@
 namespace App\Http\Controllers\Article;
 
 use App\Models\Article;
+use App\Models\Category;
 
 class IndexController extends BaseController
 {
     public function __invoke()
     {
         $articles = Article::all();
-        return view('article.index', compact('articles'));
+        $categories = Category::all();
+
+        return view('article.index', compact('articles','categories'));
     }
 }
--- app/Http/Controllers/Review/IndexController.php
+++ app/Http/Controllers/Review/IndexController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Review;
 
 use App\Http\Controllers\Review\BaseController;
 use App\Models\Review;
+use App\Models\Category;
 
 class IndexController extends BaseController
 {
     public function __invoke()
     {
         $reviews = Review::all();
-        return view('review.index', compact('reviews'));
+        $categories = Category::all();
+
+        return view('review.index', compact('reviews','categories'));
     }
 }
