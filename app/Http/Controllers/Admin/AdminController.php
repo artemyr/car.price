c19c87f22fe7245cb971043ebd28d6c4043913e1
artemyr <artem.r@affetta.ru>
2022-11-10 20:01:21 +0300

    controlls

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,11 +6,117 @@ use App\Http\Controllers\Controller;
 
 class AdminController extends Controller 
 {
-    private function getControl($type, $code) {
+    private $content = "";
 
+    private function getControl($arParams) {
+
+        $type = $arParams[0];
+        $code = $arParams[1];
+        $value = $arParams[2];
+        $name = $arParams[3];
+        if(isset($arParams[4])) $default_value = $arParams[4];
+
+        $res = "";
+        switch ($type) {
+            case 'text':
+                if(empty($value) && !empty($default_value)) $value = $default_value;
+                $res = "
+            <div class=\"admin-edit__form-control\">
+                <label for=\"". $code ."\">". $name ."</label>
+                <input name=\"". $code ."\" id=\"".$code."\" type=\"text\" value=\"". $value ."\">
+            </div>";
+                break;
+
+            case 'textarea':
+                $res = "
+            <div class=\"admin-edit__form-control\">
+                <label for=\"". $code ."\">". $name ."</label>
+                <textarea name=\"". $code ."\" id=\"". $code ."\">". $value ."</textarea>
+            </div>";
+                break;
+
+            case 'select':
+                    $res = "
+            <div class=\"admin-edit__form-control\">
+                <label for=\"category\">Category</label>
+                <select name=\"category_id\" id=\"category\">
+                ";
+                        foreach ($value as $item) {
+                            $res .= "    <option ";
+                                if($item->id == $default_value) $res .= "selected ";
+                                $res .= "value=\"". $item->id ."\">";
+                                $res .= $item->title;
+                            $res .= "</option>
+                ";
+                        }
+                    $res .= "
+                </select>
+            </div>";
+                    break;
+
+            case 'select_multiple':
+                    $res = "
+            <div class=\"admin-edit__form-control\">
+                <label for=\"". $code ."\">". $name ."</label>
+                <select multiple name=\"". $code ."\" id=\"". $code ."\">
+                ";
+                        foreach ($value as $item1) {
+                            $res .= "    <option ";                            
+                                foreach($default_value as $item2) {
+                                    if($item1->id == $item2->id) {                                        
+                                        $res .= "selected ";
+                                    }
+                                }
+        
+                                $res .= "value=\" $item->id \">";
+                                $res .= $item->title;
+                            $res .= "</option>
+                ";
+                            }
+                    $res .= "
+                </select>
+            </div>";
+                    break;
+                
+                default:
+                    # code...
+                    break;
+        }
+        return $res;
     }
 
-    private function getEditForm() {
-        echo 'loh';
+    private function addContent($content) {
+        $this->content .= $content;
+    }
+
+    private function getContent() {
+        return $this->content;
+    }
+
+    protected function getEditForm($postid, $arrControlls) {
+        $this->addContent($this->getHeader($postid));
+
+        foreach($arrControlls as $controll) {
+            $this->addContent($this->getControl($controll));
+        }
+
+        $this->addContent($this->getFooter());
+        return $this->getContent();
+    }
+    
+    private function getHeader($postid) {
+        return "<div class=\"admin-edit\">
+    <form action=\"".route('admin.post.update', $postid)."\" method=\"POST\">
+        @csrf
+        @method('patch')";
+    }
+
+    private function getFooter() {
+        return "
+        <div>
+            <input class=\"admin-edit__save\" type=\"submit\" value=\"Сохранить\">
+        </div>
+    </form>
+</div>";
     }
 }
--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -6,23 +6,61 @@ use App\Http\Controllers\Admin\Post\BaseController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
+use App\Models\City;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class EditController extends BaseController
 {
-    private function getEditForm() {
-        AdminController::getEditForm();
-    }
-
     public function __invoke (Post $post)
     {
         $posts = Post::paginate(1);
         $categorys = Category::all();
         $tags = Tag::all();
+        $cities = City::all();
 
-        dd($this->getEditForm());
+        $formControll = $this->getEditForm($post->id, [
+            [
+                'text',
+                'title',
+                $post->title, 
+                'Заголовок поста'
+            ],
+            [
+                'textarea',
+                'content',
+                $post->content, 
+                'Контент поста'
+            ],
+            [
+                'text',
+                'image',
+                $post->image, 
+                'Картинка поста'
+            ],
+            [
+                'select',
+                'category_id',
+                $categorys, 
+                'Категория поста',
+                $post->category->id
+            ],
+            [
+                'select_multiple',
+                'tags[]',
+                $tags, 
+                'Теги',
+                $post->tags
+            ],
+            [
+                'select',
+                'city',
+                $cities, 
+                'Город поста',
+                $post->city_id
+            ],
+        ]);
 
-        return view('admin.post.edit', compact('post','posts','categorys','tags'));
+        return view('admin.post.edit', compact('formControll'));
     }
 }
