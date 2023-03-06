fdcce3c15eb4456bd011d15f595566d43c0ce86c
artemyr <artem.r@affetta.ru>
2023-03-06 09:45:22 +0300

    articles

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -17,13 +17,18 @@
 
     <div class="container">
         <div class="blog-detail-pages-wrapper__wrapper">
+            <div class="blog-detail-pages-wrapper__image">
+                <img src="{{ asset($article->image_path) }}">
+            </div>
+        </div>
+        <div class="blog-detail-pages-wrapper__wrapper">
             <div class="blog-detail-pages-wrapper__content">
-                <div class="blog-detail-pages-wrapper__image">
-                    <img src="{{ asset($article->image_path) }}">
-                </div>
 
-                <div class="blog-detail-pages-wrapper__title">
-                    <div>{{ $article->title }}</div>
+                <div class="blog-detail-pages-wrapper__title">{{ $article->title }}</div>
+
+                <div class="blog-detail-pages-wrapper__title-bottom">
+                    <div>{{ $article->cr_date }}</div>
+                    <div>Socials</div>
                 </div>
 
                 <div class="blog-detail-pages-wrapper__detail-text">
@@ -32,7 +37,28 @@
 
             </div>
             <div class="blog-detail-pages-wrapper__sidebar">
-                123
+                <div class="sidebar">
+                    <div class="sidebar__head">
+                        Содержание
+                    </div>
+                    <div class="sidebar__item-wrapper">
+                        <div class="sidebar__item">
+                            1. Повседневная практика показывает
+                        </div>
+                        <div class="sidebar__item">
+                        2. Полностью перестроили IT-структуру предприятия
+                        </div>
+                        <div class="sidebar__item">
+                        3. Гарантийное и послегарантийное обслуживание оборудования бассейнов
+                        </div>
+                        <div class="sidebar__item">
+                        4. Заключение и работа по сервисным договорам
+                        </div>
+                        <div class="sidebar__item">
+                        5. Консультации по телефону по оборудованию бассейнов и уходу за водой бассейнов
+                        </div>
+                    </div>                
+                </div>
             </div>
         </div>
     </div>
